bae02cdca282ceedf3c1ec04ff4ceac1e2d669ee
ashikMostofaTonmoy <tonmoy.ashik@gmail.com>
2022-06-04 00:48:47 +0600

    downlod from xlxs file links

---
+++ downloadFileFromExcel.py
@@ -0,0 +1,79 @@
+
+# In[]:
+from dotenv import load_dotenv
+from multiprocessing.pool import ThreadPool
+from time import time
+from clint.textui import progress
+import requests
+import json
+import os
+import pandas as pd
+import sys
+import platform
+import uuid
+print(platform.python_version())
+print(sys.path)
+
+# get_ipython().system('which python')
+
+
+# In[]:
+load_dotenv('env/.env')
+
+# In[8]:
+
+
+destination = os.path.join('data', 'downloaded_all_same')
+print(destination)
+try:
+    os.makedirs(destination, exist_ok=True)
+    # print("Directory doesn't exist. \nCreating Directory...")
+except OSError as error:
+    print(error)
+
+# In[3]:
+df = pd.read_excel('data/colas_sharing_form_display.xlsx', index_col=0)
+
+
+# In[ ]:
+
+
+urlHead = os.environ.get("urlHead")
+for entry in df.index:
+    id = str(uuid.uuid4())
+    # dest = os.path.join(destination, str(entry))  # destination path
+
+    if pd.isna(entry) == False:
+        img_data = json.loads(df.loc[entry, 'img_data'])
+        # if os.path.exists(dest) == False:
+        #     try:
+        #         os.makedirs(dest, exist_ok=True)
+        #         print(f"Directory doesn't exist. \nCreating Directory: {dest} ")
+        #     except OSError as error:
+        #         print(error)
+
+        for images in img_data:
+            start = time()
+
+            # actual url of image
+            url = os.path.join(urlHead, images['img'])
+
+            # get the image name
+            head_tail = os.path.split(url)
+            frame_name = os.path.join(destination, str(entry)+'_'+head_tail[1])
+
+            # download the file
+            r = requests.get(url, stream=True)
+
+            # save the file
+            with open(frame_name, "wb") as f:
+                total_length = int(r.headers.get('content-length'))
+
+                # for progresss bar in terminal
+                for chunk in progress.bar(r.iter_content(chunk_size=2391975), expected_size=(total_length/1024) + 1):
+                    if chunk:
+                        f.write(chunk)
+            print(f"{frame_name}: took time: {time() - start :.3f}s")
+
+# In[ ]:
+# %%
---
+++ filefromexcel.py
@@ -0,0 +1,28 @@
+import os
+import requests
+# from time import time
+from multiprocessing.pool import ThreadPool
+from clint.textui import progress
+
+dest = 'data/downloaded/0'
+url1 = 'https://img.mediavitis.com/cola_images/Chardonnay_VSR2008.jpg'
+url12 = 'https://img.mediavitis.com/cola_images/backlabel.jpg'
+
+head_tail = os.path.split(url12)
+
+head_tail[1]
+frame_name = os.path.join(dest, head_tail[1])
+print(frame_name)
+url = url1
+try:
+    os.makedirs(dest, exist_ok=True)
+#     print("Directory doesn't exist. \nCreating Directory..." )
+except OSError as error:
+    print(error)
+
+r = requests.get(url, stream=True)
+with open(frame_name, "wb") as f:
+    total_length = int(r.headers.get('content-length'))
+    for chunk in progress.bar(r.iter_content(chunk_size=2391975), expected_size=(total_length/1024) + 1):
+        if chunk:
+            f.write(chunk)
